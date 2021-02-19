6243f456f57c5ee48e7a0ff2989d7739b9e62d43
Ethan Buttimer <ethan.buttimer@gmail.com>
2021-02-19 00:56:25 -0800

    subsurface option, data

--- subsTexturesUI.py
+++ subsTexturesUI.py
@@ -14,30 +14,22 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
         self.name_input = None
         self.pxr_surface_name = None
         self.directory = "none"
-        self.textures = {
-            "diffColor": "none",
-            "specFaceColor": "none",
-            "specEdgeColor": "none",
-            "specRough": "none",
-            "normal": "none",
-            "bump": "none",
-            "glowColor": "none",
-            "presence": "none",
-            "subsurfaceColor": "none"
+
+        #keys are the texture types, values are [file name, selected?, node_name]
+        self.texture_data = {
+            "diffColor": ["none", False, "none"],
+            "specFaceColor": ["none", False, "none"],
+            "specEdgeColor": ["none", False, "none"],
+            "specRough": ["none", False, "none"],
+            "normal": ["none", False, "none"],
+            "bump": ["none", False, "none"],
+            "glowColor": ["none", False, "none"],
+            "presence": ["none", False, "none"]
         }
-        self.node_names = {
-            "diffColor": "none",
-            "specFaceColor": "none",
-            "specEdgeColor": "none",
-            "specRough": "none",
-            "normal": "none",
-            "bump": "none",
-            "glowColor": "none",
-            "presence": "none",
-            "subsurfaceColor": "none"
-        }
-        self.reverse_normals = True
+
+        self.reverse_normals = False
         self.spec_mode_artistic = False
+        self.color_mode_subsurface = False
         self.loaded_existing = False
         self.buildUI()
 
@@ -97,22 +89,31 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
         self.optionsWidget = QtWidgets.QWidget()
         self.optionsLayout = QtWidgets.QVBoxLayout(self.optionsWidget)
 
+        self.diffDestLabel = QtWidgets.QLabel("Color texture destination:")
+        self.optionsLayout.addWidget(self.diffDestLabel)
+        self.diffDestWidget = QtWidgets.QWidget()
+        self.diffDestLayout = QtWidgets.QHBoxLayout(self.diffDestWidget)
+        self.diffDiffuseRadio = QRadioButton("Diffuse")
+        self.diffDiffuseRadio.setChecked(True)
+        self.diffDestLayout.addWidget(self.diffDiffuseRadio)
+        self.diffSubsurfaceRadio = QRadioButton("Subsurface")
+        self.diffDestLayout.addWidget(self.diffSubsurfaceRadio)
+        self.optionsLayout.addWidget(self.diffDestWidget)
+
         self.specDestLabel = QtWidgets.QLabel("Primary specular mode:")
         self.optionsLayout.addWidget(self.specDestLabel)
         self.specDestWidget = QtWidgets.QWidget()
         self.specDestLayout = QtWidgets.QHBoxLayout(self.specDestWidget)
-        self.specPhysicalRadio = QRadioButton("Physical")
-        self.specPhysicalRadio.toggled.connect(self.spec_toggle)
-        self.specDestLayout.addWidget(self.specPhysicalRadio)
         self.specArtisticRadio = QRadioButton("Artistic")
         self.specArtisticRadio.setChecked(True)
-        self.specArtisticRadio.toggled.connect(self.spec_toggle)
         self.specDestLayout.addWidget(self.specArtisticRadio)
+        self.specPhysicalRadio = QRadioButton("Physical")
+        self.specDestLayout.addWidget(self.specPhysicalRadio)
         self.optionsLayout.addWidget(self.specDestWidget)
 
         self.reverseCheckbox = QtWidgets.QCheckBox()
         self.reverseCheckbox.toggle()
-        self.reverseCheckbox.setText("Invert bump for normals (recommended)")
+        self.reverseCheckbox.setText("Invert bump")
         self.optionsLayout.addWidget(self.reverseCheckbox)
 
         self.layout.addWidget(self.optionsWidget)
@@ -136,9 +137,6 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
 
         self.layout.addWidget(self.btnWidget)
 
-    def spec_toggle(self):
-        self.spec_mode_artistic = not self.spec_mode_artistic
-
     def find_existing_pxr(self):
         objSel = cmds.ls(sl=True, s=1, dag=1)
         if len(objSel) == 0:
@@ -160,24 +158,22 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
             plug = connections[i]
             input = connections[i+1]
             if "diffuseColor" in plug:
-                self.node_names["diffColor"] = input
+                self.texture_data["diffColor"][2] = input
             if "specularFaceColor" in plug:
-                self.node_names["specFaceColor"] = input
+                self.texture_data["specFaceColor"][2] = input
             if "specularEdgeColor" in plug:
-                self.node_names["specEdgeColor"] = input
+                self.texture_data["specEdgeColor"][2] = input
             if "specularRoughness" in plug:
-                self.node_names["specRough"] = input
+                self.texture_data["specRough"][2] = input
             if "bumpNormal" in plug:
                 if "normal" in input:
-                    self.node_names["normal"] = input
+                    self.texture_data["normal"][2] = input
                 else:
-                    self.node_names["bump"] = input
+                    self.texture_data["bump"][2] = input
             if "glowColor" in plug:
-                self.node_names["glowColor"] = input
+                self.texture_data["glowColor"][2] = input
             if "presence" in plug:
-                self.node_names["presence"] = input
-            if "subsurfaceColor" in plug:
-                self.node_names["subsurfaceColor"] = input
+                self.texture_data["presence"][2] = input
 
         self.loaded_existing = True
 
@@ -203,33 +199,27 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
 
         for file in onlyFiles:
             if "_DiffuseColor" in file:
-                self.textures["diffColor"] = file
-                self.textures["subsurfaceColor"] = file
+                self.texture_data["diffColor"][0] = file
             if "_SpecularFaceColor" in file:
-                self.textures["specFaceColor"] = file
+                self.texture_data["specFaceColor"][0] = file
             if "_SpecularEdgeColor" in file:
-                self.textures["specEdgeColor"] = file
+                self.texture_data["specEdgeColor"][0] = file
             if "_SpecularRoughness" in file:
-                self.textures["specRough"] = file
+                self.texture_data["specRough"][0] = file
             if "_Normal" in file:
-                self.textures["normal"] = file
+                self.texture_data["normal"][0] = file
             if "_Displacement" in file:
-                self.textures["bump"] = file
+                self.texture_data["bump"][0] = file
             if "_GlowColor" in file:
-                self.textures["glowColor"] = file
+                self.texture_data["glowColor"][0] = file
             if "_Presence" in file:
-                self.textures["presence"] = file
+                self.texture_data["presence"][0] = file
 
         self.filesWidget.clear()
-        for key in self.textures:
-            if self.textures[key] != "none":
+        for key in self.texture_data:
+            if self.texture_data[key][0] != "none":
                 print "adding"
-                listText = self.textures[key]
-                if key == "diffColor":
-                    listText = ("DIFFUSE <-- %s" % self.textures[key])
-                elif key == "subsurfaceColor":
-                    listText = ("SUBSURFACE <-- %s" % self.textures[key])
-
+                listText = self.texture_data[key][0]
                 temp = QtWidgets.QListWidgetItem(listText)
                 temp.setCheckState(QtCore.Qt.Checked)
                 self.filesWidget.addItem(temp)
@@ -241,11 +231,12 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
         self.name_input = self.pxrNameField.text()
         self.directory = self.directoryField.text()
         for i in range(self.filesWidget.count()):
-            if self.filesWidget.item(i).checkState() != QtCore.Qt.Checked:
-                for key in self.textures.keys():
-                    if (self.textures[key] == self.filesWidget.item(i).text()) or (("SUBSURFACE <-- %s" % self.textures[key]) == self.filesWidget.item(i).text()) or (("DIFFUSE <-- %s" % self.textures[key]) == self.filesWidget.item(i).text()):
-                        self.textures[key] = "none"
+            if self.filesWidget.item(i).checkState() == QtCore.Qt.Checked:
+                for key in self.texture_data.keys():
+                    if (self.texture_data[key][0] == self.filesWidget.item(i).text()):
+                        self.texture_data[key][1] = True
         self.spec_mode_artistic = self.specArtisticRadio.isChecked()
+        self.color_mode_subsurface = self.diffSubsurfaceRadio.isChecked()
         self.reverse_normals = self.reverseCheckbox.isChecked()
 
     def bad_use(self):
@@ -276,12 +267,12 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
         sel = cmds.ls(sl=True)[0]
         self.pxr_surface_name = cmds.rename(sel, self.name_input)
 
-        for key in self.textures.keys():
-            if self.textures[key] != "none":
+        for key in self.texture_data.keys():
+            if self.texture_data[key][1]:
                 self.add_tex_node(key)
                 self.update_tex(key)
 
-        if self.node_names["diffColor"] == "none":
+        if !self.texture_data["diffColor"][1]:
             cmds.setAttr('%s.diffuseGain' % self.pxr_surface_name, 0.0)
 
         cmds.select(self.pxr_surface_name)
@@ -295,16 +286,14 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
         if not self.loaded_existing or not self.pxr_surface_name:
             cmds.warning("No existing PxrSurface found. Use 'Create and Assign' option.")
             return
-        if self.name_input:
-            self.pxr_surface_name = cmds.rename(self.pxr_surface_name, self.name_input)
 
-        for key in self.textures.keys():
-            if self.textures[key] != "none":
-                if self.node_names[key] == "none":
+        for key in self.texture_data.keys():
+            if self.texture_data[key][1]:
+                if self.texture_data[key][2] == "none":
                     self.add_tex_node(key)
                 self.update_tex(key)
 
-        if self.node_names["diffColor"] == "none":
+        if self.texture_data["diffColor"][2] == "none":
             cmds.setAttr('%s.diffuseGain' % self.pxr_surface_name, 0.0)
 
         cmds.select(self.pxr_surface_name)
@@ -320,9 +309,18 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
         nodeName = cmds.ls(sl=True)[0]
 
         if key == "diffColor":
-            print "adding diffuse color texture"
-            nodeName = cmds.rename(nodeName, '%s_diffuse_tex' % self.pxr_surface_name)
-            cmds.connectAttr('%s.resultRGB' % nodeName, '%s.diffuseColor' % self.pxr_surface_name, f=True)
+            if self.color_mode_subsurface:
+                print "adding subsurface color texture"
+                nodeName = cmds.rename(nodeName, '%s_subsurface_tex' % self.pxr_surface_name)
+                cmds.connectAttr('%s.resultRGB' % nodeName, '%s.subsurfaceColor' % self.pxr_surface_name, f=True)
+                cmds.setAttr('%s.subsurfaceGain' % self.pxr_surface_name, 1.0)
+                cmds.setAttr('%s.diffuseGain' % self.pxr_surface_name, 0.0)
+            else:
+                print "adding diffuse color texture"
+                nodeName = cmds.rename(nodeName, '%s_diffuse_tex' % self.pxr_surface_name)
+                cmds.connectAttr('%s.resultRGB' % nodeName, '%s.diffuseColor' % self.pxr_surface_name, f=True)
+                cmds.setAttr('%s.subsurfaceGain' % self.pxr_surface_name, 0.0)
+                cmds.setAttr('%s.diffuseGain' % self.pxr_surface_name, 1.0)
         elif key == "specFaceColor":
             if self.spec_mode_artistic:
                 print "adding specular face color texture, ARTISTIC"
@@ -349,7 +347,7 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
             nodeName = cmds.rename(nodeName, '%s_global_normal' % self.pxr_surface_name)
             cmds.connectAttr('%s.resultN' % nodeName, '%s.bumpNormal' % self.pxr_surface_name, f=True)
             cmds.setAttr('%s.invertBump' % nodeName, self.reverse_normals)
-        elif key == "bump" and self.node_names["normal"] == "none":
+        elif key == "bump" and self.texture_data["normal"][2] == "none":
             print "adding global bump texture"
             nodeName = cmds.rename(nodeName, '%s_global_bump' % self.pxr_surface_name)
             cmds.connectAttr('%s.resultN' % nodeName, '%s.bumpNormal' % self.pxr_surface_name, f=True)
@@ -362,17 +360,13 @@ class SubsTextureManagerUI(QtWidgets.QDialog):
             print "adding presence texture"
             nodeName = cmds.rename(nodeName, '%s_presence_tex' % self.pxr_surface_name)
             cmds.connectAttr('%s.resultR' % nodeName, '%s.presence' % self.pxr_surface_name, f=True)
-        elif key == "subsurfaceColor":
-            print "adding specular color texture"
-            nodeName = cmds.rename(nodeName, '%s_subsurface_tex' % self.pxr_surface_name)
-            cmds.connectAttr('%s.resultRGB' % nodeName, '%s.subsurfaceColor' % self.pxr_surface_name, f=True)
 
-        self.node_names[key] = nodeName
+        self.texture_data[key][2] = nodeName
 
     def update_tex(self, key):
-        if self.node_names[key] == "none":
+        if self.texture_data[key][2] == "none":
             return
-        cmds.setAttr('%s.filename' % self.node_names[key], os.path.join(self.directory, self.textures[key]), type='string')
+        cmds.setAttr('%s.filename' % self.texture_data[key][2], os.path.join(self.directory, self.texture_data[key][0]), type='string')
 
 
 #MAIN UI LAUNCHER
